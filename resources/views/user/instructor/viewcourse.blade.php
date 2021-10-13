eec4b47a5d623849349bdaf3dc2caf7cc956a7fe
sempermort <semperjack@gmail.com>
2021-10-13 10:58:06 +0300

    fghjk

--- resources/views/user/instructor/viewcourse.blade.php
+++ resources/views/user/instructor/viewcourse.blade.php
@@ -34,7 +34,7 @@
 
                         <div id="videotab" class="tab-pane   tabcontent h-500" >
                             <video width="400" id="mainvid" height="240"
-                                poster="{{route('get-copic',explode('/',$copic->file_path)[1])}}" class="w-100 h-100    object-contain " controls>
+                                poster="{{route('get-copic',explode('/',$copic->file_path)[1])}}" class="w-100 h-100    object-fill " controls>
                                 @if(isset($covid->id))
                                 <source src="{{route('get-covid',explode('/',$covid->file_path)[1])}}">@endif
                             </video>
