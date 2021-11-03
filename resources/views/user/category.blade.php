d605f3646cc31ebf20b66a21994530dde37baec9
sempermort <semperjack@gmail.com>
2021-11-03 08:49:15 +0300

    n

--- resources/views/user/category.blade.php
+++ resources/views/user/category.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="sa4d25  mt-20 " >
-    <div class="d-flex auto-85">
+    <div class="d-flex auto85">
         <div class=" hide-on-med-and-down d-none d-md-block d-lg-block mt-5 col-2">
             <div class="ml-2">
             <div class=" nastay position-relative">
