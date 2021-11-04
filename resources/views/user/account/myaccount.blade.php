2fbe4e43c70448d3d3c2f4b9c69bd9aa95820add
sempermort <semperjack@gmail.com>
2021-11-04 20:50:24 +0300

    gjk

--- resources/views/user/account/myaccount.blade.php
+++ resources/views/user/account/myaccount.blade.php
@@ -1,14 +1,11 @@
 @extends('layouts.stu-layout')
 
 @section('content')
+<section>
 <div class="bg-black p-5">
-
-
     <div class=" pb-1  mt-5 mb-2 ml-5 ">
         <h1 class="text-white font-weight-bolder">My Learning</h1>
     </div>
-
-
     <div class="">
         <div class="container-fluid">
             <div class="row">
@@ -34,7 +31,6 @@
         <div class="tab-pane fade show active" id="nav-about" role="tabpanel">
             <div class="_htg451">
                 <div class="_htg452">
-
                     <div class="  mb-5">
                         <div class="row justify-content-between  ">
 
@@ -44,7 +40,6 @@
 
                         <div class="row mt-3 ">
                             @foreach($wishes as $wish)
-
                             @if($wish->wishtype=="start")
                             <a href="{{route('coursedetail',$wish->course->id)}}">
                                 <div class="col-md-4">
@@ -85,200 +80,201 @@
                                                         bookmark_border
                                                     </span></small>
                                             </p>
-                                        </div>
+                                       </div>
+                                    </div>
+                                </div>
                             </a>
+                            @endif
+                            @endforeach
+
                         </div>
+
                     </div>
-                    <br>
-                    @endif
-                    @endforeach
-
                 </div>
-
             </div>
         </div>
-    </div>
-</div>
-<div class="tab-pane fade  " id="nav-purchased" role="tabpanel">
+        <div class="tab-pane fade  " id="nav-purchased" role="tabpanel">
 
-    <div class="_htg452">
-        <div class="_htg452">
+            <div class="_htg452">
+                <div class="_htg452">
 
-          <div class="mb-5">
-            <div class="row justify-content-between">
-                <h3>Purchased Courses</h3>
+                    <div class="mb-5">
+                            <div class="row justify-content-between">
+                                <h3>Purchased Courses</h3>
 
-            </div>
-
-            <div class="row">
-
-                @foreach($wishes as $wish)
-
-                @if($wish->wishtype=="start")
-                <div class="col-md-12 m-3 p-0">
-                <a href="{{route('coursedetail',$wish->course->id)}}">
-                <div class="fcrse_1 ">
-                        <div class="course-overlay rounded" style="z-index:1;">
-                                    <div class="badge_seller">Bestseller</div>
-                                    <div class="crse_reviews">
-                                        <i class="uil uil-star"></i>4.5
-                                    </div>
-                                    <span class="play_btn1">
-                                        <span class="material-icons-round ">
-                                            play_arrow
-                                        </span></i>
-                                    </span>
-                                    <div class="crse_timer">
-                                        25 hours
-                                    </div>
-                        </div>
-
-                        <div  class="hf_img">
-                            @if(isset($wish->course->mediaCover->file_path))
-                            @php
-                            $src=$wish->course->mediaCover->file_path;
-
-                            @endphp
-                            <img src="{{route('get-copic',explode('/',$src)[1])}}" alt="">
-
-                            @endif
-                        </div>
-
-                        <div class="hs_content">
-                            <div class="vdtodt">
-                                <span class="vdt14">
-                                    @php
-                                    $cnt=0;
-                                    @endphp
-                                    @foreach($totalall as $tall)
-                                    @if($tall->course_id==$wish->course_id)
-                                    @php
-                                    $cnt++;
-                                    @endphp
-                                    @endif
-                                    @endforeach
-
-                                    {{$cnt}} views</span>
-                                <span class="vdt14">15 days ago</span>
                             </div>
-                            <h5 class="text-black">{{ $wish->course->course_title }}</h5>
 
-                            <p class="crse-cate">{{$wish->course->category->category_name }}</p>
+                            <div class="row">
 
-                        </div>
-
-                </div>
-                </a>
+                                @foreach($wishes as $wish)
+                                @if($wish->wishtype=="start")
+                                <div class="col-md-12 m-3 p-0">
+                                    <a href="{{route('coursedetail',$wish->course->id)}}">
+                                        <div class="fcrse_1 ">
+                                                <div class="course-overlay rounded" style="z-index:1;">
+                                                            <div class="badge_seller">Bestseller</div>
+                                                            <div class="crse_reviews">
+                                                                <i class="uil uil-star"></i>4.5
                                                             </div>
-                @endif
-                @endforeach
+                                                            <span class="play_btn1">
+                                                                <span class="material-icons-round ">
+                                                                    play_arrow
+                                                                </span></i>
+                                                            </span>
+                                                            <div class="crse_timer">
+                                                                25 hours
+                                                            </div>
+                                                </div>
+
+                                                <div  class="hf_img">
+                                                    @if(isset($wish->course->mediaCover->file_path))
+                                                    @php
+                                                    $src=$wish->course->mediaCover->file_path;
+
+                                                    @endphp
+                                                    <img src="{{route('get-copic',explode('/',$src)[1])}}" alt="">
+
+                                                    @endif
+                                                </div>
+
+                                                <div class="hs_content">
+                                                    <div class="vdtodt">
+                                                        <span class="vdt14">
+                                                            @php
+                                                            $cnt=0;
+                                                            @endphp
+                                                            @foreach($totalall as $tall)
+                                                            @if($tall->course_id==$wish->course_id)
+                                                            @php
+                                                            $cnt++;
+                                                            @endphp
+                                                            @endif
+                                                            @endforeach
+
+                                                            {{$cnt}} views</span>
+                                                        <span class="vdt14">15 days ago</span>
+                                                    </div>
+                                                    <h5 class="text-black">{{ $wish->course->course_title }}</h5>
+
+                                                    <p class="crse-cate">{{$wish->course->category->category_name }}
+                                                    </p>
+
+                                                </div>
+
+                                        </div>
+                                    </a>
+                                </div>
+                                @endif
+                                @endforeach
+                            </div>
+                    </div>
+
                 </div>
             </div>
-
         </div>
-    </div>
-</div>
-<div class="tab-pane fade" id="nav-reviews" role="tabpanel">
-<div class="_htg452">
-        <div class="_htg452">
-        <div class="row justify-content-between">
-            <h3>Wish List</h3>
+        <div class="tab-pane fade" id="nav-reviews" role="tabpanel">
+            <div class="_htg452">
+                    <div class="_htg452">
+                        <div class="row justify-content-between">
+                            <h3>Wish List</h3>
 
-        </div>
+                        </div>
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="row mt-3">
-                @foreach($wishes as $wish)
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="row mt-3">
+                                    @foreach($wishes as $wish)
 
-                 @if($wish->wishtype=="wish")
-                    <div class="col-md-4">
-                        <div class="card cardc rounded mb-3">
+                                        @if($wish->wishtype=="wish")
+                                            <div class="col-md-4">
+                                                <div class="card cardc rounded mb-3">
 
-                            @if(isset($wish->course->mediaCover->file_path))
-                            @php
-                            $src=$wish->course->mediaCover->file_path;
+                                                    @if(isset($wish->course->mediaCover->file_path))
+                                                    @php
+                                                    $src=$wish->course->mediaCover->file_path;
 
-                            @endphp
-                            <img class="card-img-top" width="295" height="165" src="{{route('get-copic',explode('/',$src)[1])}}" alt="Card image cap">
+                                                    @endphp
+                                                    <img class="card-img-top" width="295" height="165" src="{{route('get-copic',explode('/',$src)[1])}}" alt="Card image cap">
 
-                            @endif
-                            <div class="card-img-overlay">
-                                <span class=" ">
-                                    <span class="material-icons-outlined text-white float-right">
-                                        favorite
-                                    </span>
-                                </span>
-                            </div>
-                            <div class="m-2">
+                                                    @endif
+                                                    <div class="card-img-overlay">
+                                                        <span class=" ">
+                                                            <span class="material-icons-outlined text-white float-right">
+                                                                favorite
+                                                            </span>
+                                                        </span>
+                                                    </div>
+                                                    <div class="m-2">
 
-                                <p class="m-0 bold"><a href="http://" target="_blank" rel="noopener noreferrer">Art
-                                {{ $wish->course->course_title }}</a> </p>
-                                <p class="card-text ">
-                                    <small>
-                                {{ $wish->course->course_subt }}
-                                    </small>
-                                </p>
-                                <div class="d-flex  pl-3 pr-3">
+                                                        <p class="m-0 bold"><a href="http://" target="_blank" rel="noopener noreferrer">Art
+                                                        {{ $wish->course->course_title }}</a> </p>
+                                                        <p class="card-text ">
+                                                            <small>
+                                                        {{ $wish->course->course_subt }}
+                                                            </small>
+                                                        </p>
+                                                        <div class="d-flex  pl-3 pr-3">
 
-                                    <div class="rating-box">
-                                        <span class="rating-star full-star"></span>
-                                        <span class="rating-star full-star"></span>
-                                        <span class="rating-star full-star"></span>
-                                        <span class="rating-star full-star"></span>
-                                        <span class="rating-star half-star"></span>
-                                        <div class="_rate001">4.6</div>
-                                    </div>
-                                    <div class="_rate002">(
-                                        @php
-                                    $cnt=0;
-                                    @endphp
-                                    @foreach($totalall as $tall)
-                                    @if($tall->course_id==$wish->course_id)
-                                    @php
-                                    $cnt++;
-                                    @endphp
-                                    @endif
+                                                            <div class="rating-box">
+                                                                <span class="rating-star full-star"></span>
+                                                                <span class="rating-star full-star"></span>
+                                                                <span class="rating-star full-star"></span>
+                                                                <span class="rating-star full-star"></span>
+                                                                <span class="rating-star half-star"></span>
+                                                                <div class="_rate001">4.6</div>
+                                                            </div>
+                                                            <div class="_rate002">
+                                                                (
+                                                                    @php
+                                                                $cnt=0;
+                                                                @endphp
+                                                                @foreach($totalall as $tall)
+                                                                @if($tall->course_id==$wish->course_id)
+                                                                @php
+                                                                $cnt++;
+                                                                @endphp
+                                                                @endif
+                                                                @endforeach
+
+                                                                {{$cnt}}
+                                                                    )
+                                                            </div>
+                                                        </div>
+                                                        <div class=" d-flex justify-content-end">
+                                                            <p class="card-text">
+                                                            {{$wish->course->price? "Tshs $wish->course->price":"Free"
+                                                            }}
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endif
                                     @endforeach
 
-                                    {{$cnt}}
-                                        )</div>
-                                </div>
-                                <div class=" d-flex justify-content-end">
-                                    <p class="card-text">
-                                    {{$wish->course->price? "Tshs $wish->course->price":"Free"
-                                    }}
-                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    @endif
-                @endforeach
+            </div>
+        </div>
 
+        <div class="tab-pane fade " id="nav-subscriptions" role="tabpanel">
+            <div class="student_reviews">
+                <div class="row justify-content-between">
+                    <h3></h3>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-
-<div class="tab-pane fade " id="nav-subscriptions" role="tabpanel">
-    <div class="student_reviews">
-        <div class="row justify-content-between">
-            <h3></h3>
-
-        </div>
-
-        <div class="row">
-            <div class="col-lg-12">
-               
-            </div>
-        </div>
-    </div>
-</div>
-</div>
 
 </div>
-
+</section>
 @endsection
